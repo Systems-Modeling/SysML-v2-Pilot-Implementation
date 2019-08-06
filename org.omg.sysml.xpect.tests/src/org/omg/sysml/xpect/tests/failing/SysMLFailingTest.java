ef208adc2eaa0b10a402b80cdf24c54b169a9907
Miyako Wilson <miyako.wilson@gatech.edu>
2019-08-06 13:17:32 -0400

    updated failing testcasses

---
+++ org.omg.sysml.xpect.tests/src/org/omg/sysml/xpect/tests/failing/SysMLFailingTest.java
@@ -0,0 +1,10 @@
+package org.omg.sysml.xpect.tests.failing;
+
+import org.junit.runner.RunWith;
+import org.eclipse.xpect.runner.XpectRunner;
+import org.eclipse.xpect.xtext.lib.tests.XtextTests;
+
+@RunWith(XpectRunner.class)
+public class SysMLFailingTest extends XtextTests {
+
+}

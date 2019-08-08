33b2e3e05cb1a7f4091b5ef201dee75371d48ef5
Ivan Gomes <ivan-gomes@users.noreply.github.com>
2019-08-07 23:32:12 -0700

    ST6RI-84 Added prototype of Jupyter kernel for SysML

---
+++ isysml/src/main/java/org/omg/sysml/jupyter/kernel/ISysML.java
@@ -0,0 +1,39 @@
+package org.omg.sysml.jupyter.kernel;
+
+import io.github.spencerpark.jupyter.channels.JupyterConnection;
+import io.github.spencerpark.jupyter.channels.JupyterSocket;
+import io.github.spencerpark.jupyter.kernel.KernelConnectionProperties;
+
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.logging.Level;
+
+public class ISysML {
+    public static final String LIBRARY_PATH_KEY = "ISYSML_LIBRARY_PATH";
+
+    public static void main(String[] args) throws Exception {
+        if (args.length < 1) {
+            throw new IllegalArgumentException("Missing connection file argument");
+        }
+
+        Path connectionFile = Paths.get(args[0]);
+
+        if (!Files.isRegularFile(connectionFile))
+            throw new IllegalArgumentException("Connection file '" + connectionFile + "' isn't a file.");
+
+        String contents = new String(Files.readAllBytes(connectionFile));
+
+        JupyterSocket.JUPYTER_LOGGER.setLevel(Level.WARNING);
+
+        KernelConnectionProperties connProps = KernelConnectionProperties.parse(contents);
+        JupyterConnection connection = new JupyterConnection(connProps);
+
+
+        SysMLKernel kernel = new SysMLKernel();
+        kernel.becomeHandlerForConnection(connection);
+
+        connection.connect();
+        connection.waitUntilClose();
+    }
+}
---
+++ isysml/src/main/java/org/omg/sysml/jupyter/kernel/SysMLKernel.java
@@ -0,0 +1,40 @@
+package org.omg.sysml.jupyter.kernel;
+
+import io.github.spencerpark.jupyter.kernel.BaseKernel;
+import io.github.spencerpark.jupyter.kernel.LanguageInfo;
+import io.github.spencerpark.jupyter.kernel.display.DisplayData;
+import org.omg.sysml.interactive.SysMLInteractive;
+import org.omg.sysml.interactive.SysMLInteractiveResult;
+
+import java.io.File;
+import java.util.Arrays;
+import java.util.Optional;
+
+public class SysMLKernel extends BaseKernel {
+
+    private final LanguageInfo languageInfo = new LanguageInfo.Builder("SysML").version("1.0.0").mimetype("text/x-sysml").fileExtension(".sysml").pygments("java").codemirror("sysml").build();
+
+    public SysMLKernel() {
+        Optional.of(System.getenv(ISysML.LIBRARY_PATH_KEY)).ifPresent(path -> Arrays.stream(path.split(File.pathSeparator)).forEach(SysMLInteractive.getInstance()::loadLibrary));
+    }
+
+    @Override
+    public DisplayData eval(String expr) throws Exception {
+        SysMLInteractiveResult result = SysMLInteractive.getInstance().eval(expr);
+        if (!result.getSyntaxErrors().isEmpty()) {
+            result.getSyntaxErrors().forEach(System.err::println);
+        }
+        else {
+            result.getSemanticErrors().forEach(System.err::println);
+        }
+        if (result.getException() != null) {
+            throw result.getException();
+        }
+        return new DisplayData(result.formatRootElement());
+    }
+
+    @Override
+    public LanguageInfo getLanguageInfo() {
+        return languageInfo;
+    }
+}

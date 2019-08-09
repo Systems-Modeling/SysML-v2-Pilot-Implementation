e7cab851c43d189466a1e7f2ed4ec50122e4b83f
Ed Seidewitz <ed-s@modeldriven.com>
2019-08-09 17:28:26 -0400

    ST6RI-84 Added interactive API methods show and publish.

--- org.omg.sysml/src/org/omg/sysml/interactive/SysMLInteractive.java
+++ org.omg.sysml/src/org/omg/sysml/interactive/SysMLInteractive.java
@@ -24,22 +24,36 @@
 package org.omg.sysml.interactive;
 
 import java.io.IOException;
+import java.io.PrintWriter;
+import java.io.StringWriter;
+import java.util.Date;
 import java.util.List;
 import java.util.Scanner;
 
+import org.eclipse.emf.ecore.EObject;
 import org.eclipse.emf.ecore.EPackage;
 import org.eclipse.xtext.parser.IParseResult;
+import org.eclipse.xtext.resource.IEObjectDescription;
 import org.eclipse.xtext.resource.XtextResource;
+import org.eclipse.xtext.scoping.IGlobalScopeProvider;
+import org.eclipse.xtext.scoping.IScope;
 import org.eclipse.xtext.util.CancelIndicator;
 import org.eclipse.xtext.validation.CheckMode;
 import org.eclipse.xtext.validation.IResourceValidator;
 import org.eclipse.xtext.validation.Issue;
 import org.omg.sysml.AlfStandaloneSetup;
+import org.omg.sysml.ApiException;
 import org.omg.sysml.lang.sysml.Element;
 import org.omg.sysml.lang.sysml.SysMLPackage;
 import org.omg.sysml.lang.sysml.util.SysMLLibraryUtil;
+import org.omg.sysml.naming.AlfQualifiedNameConverter;
 import org.omg.sysml.util.AlfUtil;
+import org.omg.sysml.util.traversal.Traversal;
+import org.omg.sysml.util.traversal.facade.impl.ApiElementProcessingFacade;
+import org.omg.sysml.util.traversal.impl.TraversalImpl;
+import org.omg.sysml.util.traversal.visitor.impl.ElementVisitorFactoryImpl;
 
+import com.google.common.base.Predicate;
 import com.google.inject.Inject;
 import com.google.inject.Injector;
 
@@ -49,14 +63,20 @@ public class SysMLInteractive extends AlfUtil {
 	
 	protected int counter = 1;
 	protected XtextResource resource;
-	protected String fileName = "interactive";
+	
+	protected Traversal traversal;
+	
+	@Inject
+	private IGlobalScopeProvider scopeProvider;
+	
+	@Inject
+	private AlfQualifiedNameConverter qualifiedNameConverter;
 	
 	@Inject
 	private IResourceValidator validator;
 	
 	@Inject
 	private SysMLInteractive() {
-		super(new InverseOrderedResourceSetImpl());
 		EPackage.Registry.INSTANCE.put(SysMLPackage.eNS_URI, SysMLPackage.eINSTANCE);
 	}
 	
@@ -67,12 +87,9 @@ public class SysMLInteractive extends AlfUtil {
 		}
 	}
 	
-	public void setFileName(String fileName) {
-		this.fileName = fileName;
-	}
-	
 	public int next() {
 		this.resource = (XtextResource)this.createResource(this.counter + ALF_EXTENSION);
+		this.addInputResource(this.resource);
 		return this.counter++;
 	}
 	
@@ -103,6 +120,68 @@ public class SysMLInteractive extends AlfUtil {
 		}
 	}
 	
+	public Element resolve(String name) {
+		if (this.resource != null) {
+			IScope scope = scopeProvider.getScope(
+					this.resource, SysMLPackage.eINSTANCE.getPackage_Member(), 
+					new Predicate<IEObjectDescription>() {
+	
+						@Override
+						public boolean apply(IEObjectDescription description) {
+							EObject object = description.getEObjectOrProxy();
+							return inputResources.contains(object.eResource());
+						}
+					
+					});
+			IEObjectDescription description = scope.getSingleElement(
+					this.qualifiedNameConverter.toQualifiedName(name));
+			if (description != null) {
+				EObject object = description.getEObjectOrProxy();
+				return object instanceof Element? (Element)object: null;
+			}
+		}
+		return null;
+	}
+	
+	public String show(String name) {
+		this.counter++;
+		try {
+			Element element = this.resolve(name);
+			return element == null? "ERROR:Couldn't resolve reference to Element '" + name + "'":
+					new SysMLInteractiveResult(this.resolve(name), null).formatRootElement();
+		} catch (Exception e) {
+			return new SysMLInteractiveResult(e).formatException();
+		}
+	}
+	
+	protected static ApiElementProcessingFacade getProcessingFacade(String modelName) throws ApiException {
+		ApiElementProcessingFacade processingFacade = new ApiElementProcessingFacade(modelName);	
+		final ElementVisitorFactoryImpl visitorFactory = new ElementVisitorFactoryImpl(processingFacade);
+		Traversal traversal = new TraversalImpl(visitorFactory);
+		visitorFactory.setTraversal(traversal);	
+		processingFacade.setTraversal(traversal);
+		return processingFacade;
+	}
+	
+	public String publish(String name) {
+		this.counter++;
+		try {
+			Element element = this.resolve(name);
+			if (element == null) {
+				return "ERROR:Couldn't resolve reference to Element '" + name + "'";
+			} else {
+				String modelName = element.getName() + " " + new Date();
+				ApiElementProcessingFacade processingFacade = getProcessingFacade(modelName);
+				processingFacade.getTraversal().visit(element);
+				return modelName + " (" + processingFacade.getModelId() + ")";
+			}
+		} catch (Exception e) {
+			StringWriter writer = new StringWriter();
+			e.printStackTrace(new PrintWriter(writer));
+			return writer.toString();
+		}
+	}
+	
 	public void run(String input) {
 		if (input != null && !input.isEmpty()) {
 			System.out.print(this.eval(input));
@@ -113,22 +192,37 @@ public class SysMLInteractive extends AlfUtil {
         try (Scanner in = new Scanner(System.in)) {
 	        do {
 	        	System.out.print(this.counter + "> ");
-	        	String input = in.nextLine();
-	        	if ("\\\\".equals(input.trim())) {
-	        		break;
-	        	} else {
-	        		if ("\\".equals(input.trim())) {
+	        	String input = in.nextLine().trim();
+	        	if (input.startsWith("%")) {
+	        		if ("%".equals(input)) {
 	        			input = "";
 	        			String line = in.nextLine();
-	        			while (!"\\".equals(line.trim())) {
-	        				if ("\\\\".equals(line.trim())) {
+	        			while (!"%".equals(line.trim())) {
+	        				if ("%%".equals(line.trim())) {
 	        					input = null;
 	        					break;
 	        				}
 	        				input += line + "\n";
 	        				line = in.nextLine();
 	        			}
+		        		run(input);
+	        		} else {
+	        			String[] tokens = input.split("\\s");
+	        			if ("%exit".equals(tokens[0])) {
+	        				break;
+	        			} else if ("%show".equals(tokens[0])) {
+	        				if (tokens.length > 1) {
+	        					System.out.println(this.show(tokens[1]));
+	        				}
+	        			} else if ("%publish".equals(tokens[0])) {
+	        				if (tokens.length > 1) {
+	        					System.out.println(this.publish(tokens[1]));
+	        				}
+	        			} else {
+	        				System.out.println("ERROR:Invalid command '" + input + "'");
+	        			}
 	        		}
+	        	} else {
 	        		run(input);
 	        	}
 	        } while(true);

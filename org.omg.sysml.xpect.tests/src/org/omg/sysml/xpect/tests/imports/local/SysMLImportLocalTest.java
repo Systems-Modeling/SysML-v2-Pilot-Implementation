16f6524a5e8a9f1d77b504551fe7a68300537da7
Miyako Wilson <miyako.wilson@gatech.edu>
2019-08-30 12:49:21 -0400

    Rename the test class

---
+++ org.omg.sysml.xpect.tests/src/org/omg/sysml/xpect/tests/imports/local/SysMLImportLocalTest.java
@@ -0,0 +1,9 @@
+package org.omg.sysml.xpect.tests.imports.local;
+
+import org.junit.runner.RunWith;
+import org.eclipse.xpect.runner.XpectRunner;
+import org.eclipse.xpect.xtext.lib.tests.XtextTests;
+
+@RunWith(XpectRunner.class)
+public class SysMLImportLocalTest extends XtextTests {
+}

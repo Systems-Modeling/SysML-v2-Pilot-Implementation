7e2db078357d4828ead911f9fae8b12b7e3461a1
Miyako Wilson <miyako.wilson@gatech.edu>
2019-08-30 12:50:35 -0400

    include org.omg.sysml.xpects.imports.local tests to the all test.

--- org.omg.sysml.xpect.tests/src/org/omg/sysml/xpect/tests/AllSysMLXpectTests.java
+++ org.omg.sysml.xpect.tests/src/org/omg/sysml/xpect/tests/AllSysMLXpectTests.java
@@ -4,6 +4,7 @@ import org.junit.runner.RunWith;
 import org.junit.runners.Suite;
 import org.junit.runners.Suite.SuiteClasses;
 import org.omg.sysml.xpect.tests.generator.SysMLGeneratorTest;
+import org.omg.sysml.xpect.tests.imports.local.SysMLImportLocalTest;
 import org.omg.sysml.xpect.tests.linking.SysMLLinkingTest;
 import org.omg.sysml.xpect.tests.parser.SysMLParserTest;
 import org.omg.sysml.xpect.tests.scoping.SysMLScopingTest;
@@ -11,8 +12,10 @@ import org.omg.sysml.xpect.tests.testsuite.SysMLXtextTest;
 import org.omg.sysml.xpect.tests.validation.SysMLValidationTest;
 
 @SuiteClasses({ SysMLLinkingTest.class, //
-		SysMLParserTest.class, //
 		SysMLGeneratorTest.class, //
+		SysMLImportLocalTest.class, //
+		SysMLLinkingTest.class,//
+		SysMLParserTest.class, //
 		SysMLScopingTest.class, //
 		SysMLXtextTest.class, //
 		SysMLValidationTest.class //

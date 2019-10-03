a41d5436154fe7af2d1a3da17916cb0d22160fff
Miyako Wilson <miyako.wilson@gatech.edu>
2019-10-02 21:42:18 -0400

    create the failing package because the scope seems wrong.

---
+++ org.omg.sysml.xpect.tests/src/org/omg/sysml/xpect/tests/imports/global/failing/SysMLImportGlobalFailingTest.java
@@ -0,0 +1,9 @@
+package org.omg.sysml.xpect.tests.imports.global.failing;
+
+import org.junit.runner.RunWith;
+import org.eclipse.xpect.runner.XpectRunner;
+import org.eclipse.xpect.xtext.lib.tests.XtextTests;
+
+@RunWith(XpectRunner.class)
+public class SysMLImportGlobalFailingTest extends XtextTests {
+}

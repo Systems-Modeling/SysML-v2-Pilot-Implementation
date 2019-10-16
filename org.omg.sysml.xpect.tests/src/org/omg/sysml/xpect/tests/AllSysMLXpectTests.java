cb4525229c4f7461148e49d3c3c25db1b6b9ea76
Miyako Wilson <miyako.wilson@gatech.edu>
2019-10-16 12:58:10 -0400

    Add global import test

--- org.omg.sysml.xpect.tests/src/org/omg/sysml/xpect/tests/AllSysMLXpectTests.java
+++ org.omg.sysml.xpect.tests/src/org/omg/sysml/xpect/tests/AllSysMLXpectTests.java
@@ -4,6 +4,7 @@ import org.junit.runner.RunWith;
 import org.junit.runners.Suite;
 import org.junit.runners.Suite.SuiteClasses;
 import org.omg.sysml.xpect.tests.generator.SysMLGeneratorTest;
+import org.omg.sysml.xpect.tests.imports.global.SysMLImportGlobalTest;
 import org.omg.sysml.xpect.tests.imports.local.SysMLImportLocalTest;
 import org.omg.sysml.xpect.tests.linking.SysMLLinkingTest;
 import org.omg.sysml.xpect.tests.parser.SysMLParserTest;
@@ -13,6 +14,7 @@ import org.omg.sysml.xpect.tests.validation.SysMLValidationTest;
 
 @SuiteClasses({ SysMLLinkingTest.class, //
 		SysMLGeneratorTest.class, //
+		SysMLImportGlobalTest.class, //
 		SysMLImportLocalTest.class, //
 		SysMLLinkingTest.class,//
 		SysMLParserTest.class, //

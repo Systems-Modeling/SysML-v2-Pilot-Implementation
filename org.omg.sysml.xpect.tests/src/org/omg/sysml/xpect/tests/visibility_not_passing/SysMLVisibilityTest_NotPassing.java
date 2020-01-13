225f4a37eaf09da9f5dc9a11190a9441c855d297
Ed Seidewitz <ed-s@modeldriven.com>
2020-01-13 14:03:53 -0500

    ST6RI-41 Updated VisibilityTests_Import_Scope_0 so it fails w/o errors.

--- org.omg.sysml.xpect.tests/src/org/omg/sysml/xpect/tests/visibility_not_passing/SysMLVisibilityTest_NotPassing.java
+++ org.omg.sysml.xpect.tests/src/org/omg/sysml/xpect/tests/visibility_not_passing/SysMLVisibilityTest_NotPassing.java
@@ -2,11 +2,11 @@ package org.omg.sysml.xpect.tests.visibility_not_passing;
 
 import org.eclipse.xpect.runner.XpectRunner;
 import org.eclipse.xpect.runner.XpectTestFiles;
-import org.eclipse.xpect.xtext.lib.tests.ValidationTest;
+import org.eclipse.xpect.xtext.lib.tests.XtextTests;
 import org.junit.runner.RunWith;
 
 @RunWith(XpectRunner.class)
 @XpectTestFiles(fileExtensions = "xt")
-public class SysMLVisibilityTest_NotPassing extends ValidationTest {
+public class SysMLVisibilityTest_NotPassing extends XtextTests {
 
 }

4d5f3d7f29394720ddc5c5810540d274ce9e21ec
Ed Seidewitz <ed-s@modeldriven.com>
2020-04-06 18:37:47 -0400

    Prepared for JupyterHub deployment.

--- org.omg.sysml.jupyter/src/main/resources/kernel/kernel.js
+++ org.omg.sysml.jupyter/src/main/resources/kernel/kernel.js
@@ -24,39 +24,26 @@ var enableMode = function (CodeMirror) {
     CodeMirror.defineMode("sysml", function (config) {
         return CodeMirror.getMode(config, {
             name: "clike",
-            keywords: words("about abstract accept action activity alias allInstances any as assert assoc bind block by " +
-                "comment connect constraint decide def do else end entry exhibit exit first flow fork from hastype if import " +
-                "in inout instanceof interface is join link merge nonunique of ordered out package part perform port private " +
-                "protected public redefines ref send specializes state stream subsets succession then to transition type value"),
-            defKeywords: words("action activity assoc block constraint def link interface package part port ref state transition type value"),
+            keywords: words("about abstract accept action activity alias allInstances any as assert assoc assume bind block by " +
+                "comment connect constraint decide def do else end entry exhibit exit first flow fork from hastype id if import " +
+                "in inout instanceof interface individual join link merge nonunique of ordered out package part perform port private " +
+                "protected public redefines ref require requirement satisfy send snapshot specializes state stream subsets succession then " + 
+                "timeslice to transition type typed value"),
+            defKeywords: words("action activity assoc block constraint def id link interface package part port ref state transition type value"),
             typeFirstDefinitions: true,
             atoms: words("true false null"),
             number: /^(?:0x[a-f\d_]+|0b[01_]+|(?:[\d_]+\.?\d*|\.\d+)(?:e[-+]?[\d_]+)?)(u|ll?|l|f)?/i,
-            modeProps: {fold: ["brace"]}
-            /*
-            keywords: words("abstract assert break case catch class const continue default " +
-                "do else enum extends final finally for goto if implements import " +
-                "instanceof interface native new package private protected public " +
-                "return static strictfp super switch synchronized this throw throws transient " +
-                "try volatile while @interface"),
-            types: words("byte short int long float double boolean char void Boolean Byte Character Double Float " +
-                "Integer Long Number Object Short String StringBuffer StringBuilder Void"),
-            blockKeywords: words("catch class do else finally for if switch try while"),
-            defKeywords: words("class interface enum @interface"),
-            typeFirstDefinitions: true,
-            atoms: words("true false null"),
-            number: /^(?:0x[a-f\d_]+|0b[01_]+|(?:[\d_]+\.?\d*|\.\d+)(?:e[-+]?[\d_]+)?)(u|ll?|l|f)?/i,
+            modeProps: {fold: ["brace"]},
             hooks: {
-                "@": function(stream) {
-                    // Don't match the @interface keyword.
-                    if (stream.match('interface', false)) return false;
-
-                    stream.eatWhile(/[\w\$_]/);
-                    return "meta";
-                }
-            },
-            modeProps: {fold: ["brace", "import"]}
-             */
+				"'": function(stream) {
+						var escaped = false, next, end = false;
+						while ((next = stream.next()) != null) {
+							if (next == "'" && !escaped) { break; }
+							escaped = !escaped && next == "\\";
+						}
+						return "variable";
+				}
+			}
         });
     });
     CodeMirror.defineMIME("text/x-sysml", "sysml");

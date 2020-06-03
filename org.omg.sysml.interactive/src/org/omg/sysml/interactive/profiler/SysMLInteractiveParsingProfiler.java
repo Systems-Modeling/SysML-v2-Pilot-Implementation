e403a78341296f6e15043058f9caefead4ef8c00
Zoltan Ujhelyi <zoltan.ujhelyi@incquerylabs.com>
2020-06-03 18:43:01 +0200

    ST6RI-212 Restructures interactive parser profiler
    This change moves and renames the provided parser profiler application
    and extends it capabilities to accept multiple model files as input
    parameters.

--- org.omg.sysml.interactive/src/org/omg/sysml/interactive/profiler/SysMLInteractiveParsingProfiler.java
+++ org.omg.sysml.interactive/src/org/omg/sysml/interactive/profiler/SysMLInteractiveParsingProfiler.java
@@ -21,30 +21,50 @@
  *  Zoltan Ujhelyi, MDS
  * 
  *****************************************************************************/
-package org.omg.sysml.interactive;
+package org.omg.sysml.interactive.profiler;
 
 import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.util.concurrent.TimeUnit;
 import java.util.stream.Collectors;
 
+import org.omg.sysml.interactive.SysMLInteractive;
+import org.omg.sysml.interactive.SysMLInteractiveResult;
+
 import com.google.common.base.Stopwatch;
 
-public class SysMLInteractiveParsingTest {
+/**
+ * This class provides a reusable application for profiling the interactive parser component of the SysML. The profiler  
+ *
+ */
+public class SysMLInteractiveParsingProfiler {
 
 	public static void main(String[] args) throws Exception {
+		Stopwatch initWatch = Stopwatch.createStarted();
 		SysMLInteractive instance = SysMLInteractive.getInstance();
-		if (args.length > 0) {
-			instance.loadLibrary(args[0]);
+		if (args.length <= 1) {
+			System.out.println("Usage: ");
+			System.out.println("SysMLInteractiveParsingProfiler <LIBRARY FOLDER> <FILENAME>");
 		}
-		Stopwatch watch = Stopwatch.createStarted();
-		SysMLInteractiveResult results = instance.eval(Files.lines(Paths.get(args[1])).collect(Collectors.joining("\n")));
-		if (results.hasErrors()) {
-			System.out.println(results.formatIssues());
-		} else {
-			System.out.println(results.formatRootElement());
+		instance.loadLibrary(args[0]);
+		
+		initWatch.stop();
+		System.out.println("Libraries loaded in " + initWatch.elapsed(TimeUnit.MILLISECONDS) + " ms");
+		System.out.println();
+		
+		for (int i = 1; i < args.length; i++) {
+			System.out.println("Loading input " + args[i]);
+			Stopwatch watch = Stopwatch.createStarted();
+			SysMLInteractiveResult results = instance.eval(Files.lines(Paths.get(args[i])).collect(Collectors.joining("\n")));
+			if (results.hasErrors()) {
+				System.out.println(results.formatIssues());
+			} else {
+				System.out.println(results.formatRootElement());
+			}
+			watch.stop();
+			System.out.println(watch.elapsed(TimeUnit.MILLISECONDS) + " ms");
+			System.out.println();
 		}
-		System.out.println(watch.elapsed(TimeUnit.MILLISECONDS));
 	}
 
 }

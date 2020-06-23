6eed001d8fd64636cc270d13c7fd012397b7fbad
Hans Peter de Koning <hanspeter.dekoning@gmail.com>
2020-06-23 22:11:11 +0200

    ST6RI-233 Added tool-support with basic syntax highlighting capabilities for VS Code and JetBrains IDEs
    - Includes xtext_grammar_converter.py Python script to convert XText grammars
      automatically into VS Code and JetBrains IDE language definition files

---
+++ tool-support/syntax-highlighting/README.md
@@ -0,0 +1,99 @@
+# KerML and SysML language definitions for non-Eclipse IDEs
+
+This project contains language definition files to enable syntax highlighting in IDEs other than Eclipse.
+
+The language definition files for other IDES are auto-generated. Currently the following IDEs are supported:
+- *Visual Studio Code*
+- *JetBrains* IDEs, e.g. *PyCharm*
+ 
+## Installation for Visual Studio Code (VS Code)
+
+In order to use the KerML and SysML grammars for syntax highlighting with *VS Code* perform the following steps:
+
+1. Install the latest **VS Code** from https://code.visualstudio.com/
+2. Open the `extensions` installation folder
+   - On Windows: `%localappdata%\Programs\Microsoft VS Code\resources\app\extensions`
+   - On MacOS: `/Applications/Visual Studio Code.app/Contents/Resources/app/extensions`
+3. Copy the folders `kerml` and `sysml` from the `vscode` folder into the `extensions` installation folder
+4. Start **VS Code**
+
+If you want similar colors as in the SysML training examples, add the following settings to **VS CODE**:
+- Select menu *File > Preferences > Settings*
+- Select the tab *User* for global settings or *Workspace* for settings limited to the current project
+- In the *Search Bar* at the top enter `Token Color`
+- In the search results click `Edit in settings.json` under *Token Color Customizations*
+
+Once the `settings.json` file is open, insert the following snippet:
+```json
+  "editor.tokenColorCustomizations": {
+    "comments": "#20a060",
+    "variables": "#0000ff",
+    "strings": "#c00000",
+    "textMateRules": [
+      {
+        "scope": "keyword",
+        "settings": {
+          "foreground": "#7b0050",
+          "fontStyle": "bold"
+        }
+      },
+      {
+        "scope": "keyword.operator",
+        "settings": {
+          "foreground": "#7b0050",
+          "fontStyle": "bold"
+        }
+      },
+      {
+        "scope": "keyword.control",
+        "settings": {
+          "foreground": "#7b0050",
+          "fontStyle": "bold"
+        }
+      }
+    ]
+  },
+```
+... and save the file.
+
+Open a `.kerml` source file and a `.sysml` source file to verify that syntax coloring works.
+
+## Installation for JetBrains IDEs
+
+In order to use the KerML and SysML grammars for syntax highlighting with a *JetBrains IDE* perform the following steps:
+
+1. Close any running *JetBrains IDE*.
+2. Open the `filetypes` settings folder:
+   - On Windows: `%APPDATA%\JetBrains\<product><version>\settingsRepository\repository\filetypes`
+   - On MacOS: `~/Library/Application Support/JetBrains/<product><version>/settingsRepository/repository/filetypes`
+   - E.g. for *PyCharm version 2020.1* `<product><version>` is `PyCharm2020.1`.
+3. Open `MY_GIT_FOLDER/SysML-v2-Pilot-Implementation/tool-support/syntax-highlighting/jetbrains`.
+4. Copy the files `KerML.xml` and `SysML.xml` from above `jetbrains` folder into the `filetypes` folder.
+5. Start your *JetBrains IDE*.
+
+If needed, you can experiment with the language settings through menu *File > Settings ... > Editor > File Types*. 
+Look for the *OMG Kernel Modeling Language* or *OMG Systems Modeling Language* settings.
+
+## Update TextMate and JetBrains Language Definition Files
+
+To update the language definition files from the `KerML.xtext` and `SysML.xtext` source grammars 
+in their respective repository folders, run the Python (v3) script `xtext_grammar_converter.py` as follows:
+
+    $ cd MY_GIT_FOLDER/SysML-v2-Pilot-Implementation/tool-support/syntax-highlighting
+    $ python xtext_grammar_converter.py
+
+This updates the following language definition files:
+- `MY_GIT_FOLDER/SysML-v2-Pilot-Implementation/tool-support/syntax-highlighting/vscode/kerml/syntaxes/kerml.tmLanguage.json`
+- `MY_GIT_FOLDER/SysML-v2-Pilot-Implementation/tool-support/syntax-highlighting/vscode/sysml/syntaxes/sysml.tmLanguage.json`
+- `MY_GIT_FOLDER/SysML-v2-Pilot-Implementation/tool-support/syntax-highlighting/jetbrains/KerML.xml`
+- `MY_GIT_FOLDER/SysML-v2-Pilot-Implementation/tool-support/syntax-highlighting/jetbrains/SysML.xml`
+
+## Known Issues
+
+None.
+
+## Release Notes
+
+### Release 2020-06
+
+2020-06-22: First release, based on the 2020-06 release of the KerML and SysML `xtext` language definitions.
---
+++ tool-support/syntax-highlighting/xtext_grammar_converter.py
@@ -0,0 +1,308 @@
+"""
+Basic TextMate grammar for kerml and sysml language as part of SysML 2 Pilot Implementation
+
+Copyright (c) 2020 DEKonsult
+
+Purpose: Syntax highlighting in VS Code  editor similar to Jupyter Notebook
+
+Contributor(s):
+- Hans Peter de Koning (DEKonsult)
+"""
+
+import sys
+import os
+import re
+
+
+class DATA:
+    LANGUAGE_NAME_DICT = {
+        "KerML": "OMG Kernel Modeling Language",
+        "SysML": "OMG Systems Modeling Language",
+    }
+
+    """
+    Data template for TextMate language configuration file
+    """
+    TM_LANGUAGE_JSON_TEMPLATE = """\
+{{
+  "$schema": "https://raw.githubusercontent.com/martinring/tmlanguage/master/tmlanguage.json",
+  "name": "{LANGUAGE_NAME}",
+  "patterns": [
+    {{
+      "include": "#comments"
+    }},
+    {{
+      "include": "#keywords"
+    }},
+    {{
+      "include": "#quoted-variables"
+    }},
+    {{
+      "include": "#strings"
+    }}
+  ],
+  "repository": {{
+    "keywords": {{
+      "patterns": [
+        {{ "match": "\\\\b({KEYWORDS})\\\\b", "name": "keyword.other.{LANGUAGE_NAME_LOWERCASE}" }},
+        {{ "match": "({OPERATORS_LOGICAL})", "name": "keyword.operator.logical.{LANGUAGE_NAME_LOWERCASE}" }},
+        {{ "match": "({OPERATORS_COMPARISON})", "name": "keyword.operator.comparison.{LANGUAGE_NAME_LOWERCASE}" }},
+        {{ "match": "({OPERATORS_ARITHMETIC})", "name": "keyword.operator.arithmetic.{LANGUAGE_NAME_LOWERCASE}" }},
+        {{ "match": "({OPERATORS_OTHER})", "name": "keyword.operator.other.{LANGUAGE_NAME_LOWERCASE}" }}
+      ]
+    }},
+    "strings": {{
+      "name": "string.quoted.double.{LANGUAGE_NAME_LOWERCASE}",
+      "begin": "\\"",
+      "end": "\\"",
+      "patterns": [
+        {{
+          "name": "constant.character.escape.{LANGUAGE_NAME_LOWERCASE}",
+          "match": "\\\\\\\\."
+        }}
+      ]
+    }},
+    "quoted-variables": {{
+      "patterns": [{{ "match": "\\\\b(\\\\'[^']+\\\\')\\\\b", "name": "variable.name.quoted.{LANGUAGE_NAME_LOWERCASE}" }}]
+    }},
+    "comments": {{
+      "patterns": [
+        {{
+          "captures": {{
+            "0": {{
+              "name": "punctuation.definition.comment.{LANGUAGE_NAME_LOWERCASE}"
+            }}
+          }},
+          "match": "/\\\\*\\\\*/",
+          "name": "comment.block.empty.{LANGUAGE_NAME_LOWERCASE}"
+        }},
+        {{
+          "include": "#comments-inline"
+        }}
+      ]
+    }},
+    "comments-inline": {{
+      "patterns": [
+        {{
+          "begin": "/\\\\*",
+          "captures": {{
+            "0": {{
+              "name": "punctuation.definition.comment.{LANGUAGE_NAME_LOWERCASE}"
+            }}
+          }},
+          "end": "\\\\*/",
+          "name": "comment.block.{LANGUAGE_NAME_LOWERCASE}"
+        }},
+        {{
+          "begin": "(^[ \\\\t]+)?(?=//)",
+          "beginCaptures": {{
+            "1": {{
+              "name": "punctuation.whitespace.comment.leading.{LANGUAGE_NAME_LOWERCASE}"
+            }}
+          }},
+          "end": "(?!\\\\G)",
+          "patterns": [
+            {{
+              "begin": "//",
+              "beginCaptures": {{
+                "0": {{
+                  "name": "punctuation.definition.comment.{LANGUAGE_NAME_LOWERCASE}"
+                }}
+              }},
+              "end": "\\\\n",
+              "name": "comment.line.double-slash.{LANGUAGE_NAME_LOWERCASE}"
+            }}
+          ]
+        }}
+      ]
+    }}
+  }},
+  "scopeName": "source.{LANGUAGE_NAME_LOWERCASE}"
+}}
+"""
+
+    """
+    Data template for JetBrains language configuration file
+    """
+    JETBRAINS_LANGUAGE_TEMPLATE = """\
+<filetype binary="false" description="{LANGUAGE_NAME_FULL}" name="{LANGUAGE_NAME}">
+  <highlighting>
+    <options>
+      <option name="LINE_COMMENT" value="//" />
+      <option name="COMMENT_START" value="/*" />
+      <option name="COMMENT_END" value="*/" />
+      <option name="HEX_PREFIX" value="" />
+      <option name="NUM_POSTFIXES" value="" />
+      <option name="HAS_BRACES" value="true" />
+      <option name="HAS_BRACKETS" value="true" />
+      <option name="HAS_PARENS" value="true" />
+      <option name="HAS_STRING_ESCAPES" value="true" />
+    </options>
+    <keywords keywords="{KEYWORDS}" ignore_case="false" />
+    <keywords2 keywords="{OPERATORS}" />
+  </highlighting>
+  <extensionMap>
+    <mapping ext="{LANGUAGE_NAME_LOWERCASE}" />
+  </extensionMap>
+</filetype>
+"""
+
+
+class Converter(object):
+    """
+    Converter from xtext grammar definition file to TextMate grammar
+    """
+
+    def __init__(self):
+        self.language_name: str = ""
+
+        self.keywords: [str] = []
+        self.brackets: [str] = []
+        self.operators: [str] = []
+        self.operators_logical: [str] = []
+        self.operators_comparison: [str] = []
+        self.operators_arithmetic: [str] = []
+        self.operators_other: [str] = []
+
+    def parse_xtext_grammar(self, xtext_source_path: str):
+        try:
+            input_file = open(xtext_source_path, mode="r")
+        except IOError as e:
+            print(e)
+            sys.exit(1)
+        print(f"INFO: Parsing {xtext_source_path}")
+
+        self.language_name, extension = os.path.splitext(os.path.basename(xtext_source_path))
+
+        xtext_grammar = input_file.read()
+        input_file.close()
+
+        xtext_grammar = xtext_grammar.replace("';'", "@SEMICOLON@").replace("'*/'", "@END_COMMENT@").replace("*/", ";")
+
+        xtext_statements = [stat.replace("@SEMICOLON@", "';'").replace("@END_COMMENT@", "'*/'").strip()
+                            for stat in xtext_grammar.split(";")]
+
+        single_quote_string_pattern = re.compile(r"['][^']+[']")
+        bracket_like_char_pattern = re.compile(r"^(\(|\)|\[|\]|\{|\})")
+
+        keywords_set = set()
+        brackets_set = set()
+        operators_set = set()
+
+        # Collect token literals up to the TERMINALS section
+        for statement in xtext_statements:
+            if statement.startswith("/* TERMINALS"):
+                break
+            raw_tokens = single_quote_string_pattern.findall(statement)
+            for raw_token in raw_tokens:
+                token = raw_token[1:-1].strip()
+                if token.isidentifier():
+                    keywords_set.add(token)
+                elif bracket_like_char_pattern.match(token):
+                    brackets_set.add(token)
+                else:
+                    operators_set.add(token)
+
+        # Remove interpunction terminals that should not be highlighted
+        operators_set.remove(".")
+        operators_set.remove(",")
+        operators_set.remove(";")
+
+        self.keywords = sorted(keywords_set)
+        self.brackets = sorted(brackets_set)
+        self.operators = sorted(operators_set)
+
+        self.operators_logical = []
+        self.operators_comparison = []
+        self.operators_arithmetic = []
+        self.operators_other = []
+
+        for operator in self.operators:
+            if operator in ("@", "~", "->") or operator[0] in (":", "."):
+                self.operators_other.append(operator)
+            elif operator[0] in ("=", "<", ">", "!"):
+                self.operators_comparison.append(operator)
+            elif operator[0] in ("+", "-", "*", "/"):
+                self.operators_arithmetic.append(operator)
+            else:
+                self.operators_logical.append(operator)
+
+    def export_text_mate_language_file(self, target_path: str):
+
+        text_mate_language_definition = DATA.TM_LANGUAGE_JSON_TEMPLATE.format(
+            LANGUAGE_NAME=self.language_name,
+            LANGUAGE_NAME_LOWERCASE=self.language_name.lower(),
+            KEYWORDS="|".join(self.keywords),
+            OPERATORS_LOGICAL="|".join([Converter.escape_regex(op) for op in self.operators_logical]),
+            OPERATORS_COMPARISON="|".join([Converter.escape_regex(op) for op in self.operators_comparison]),
+            OPERATORS_ARITHMETIC="|".join([Converter.escape_regex(op) for op in self.operators_arithmetic]),
+            OPERATORS_OTHER="|".join([Converter.escape_regex(op) for op in self.operators_other])
+        )
+
+        output_file = open(target_path, "w")
+        output_file.write(text_mate_language_definition)
+        output_file.close()
+
+        print(f"INFO: Saved generated TextMate grammar in {target_path}")
+
+    def export_jetbrains_language_file(self, target_path: str):
+
+        jetbrains_language_definition = DATA.JETBRAINS_LANGUAGE_TEMPLATE.format(
+            LANGUAGE_NAME=self.language_name,
+            LANGUAGE_NAME_LOWERCASE=self.language_name.lower(),
+            LANGUAGE_NAME_FULL=DATA.LANGUAGE_NAME_DICT[self.language_name],
+            KEYWORDS=";".join(self.keywords),
+            OPERATORS=";".join([Converter.convert_xml_special(op) for op in self.operators])
+        )
+
+        output_file = open(target_path, "w")
+        output_file.write(jetbrains_language_definition)
+        output_file.close()
+
+        print(f"INFO: Saved generated JetBrains language definition in {target_path}")
+
+    @staticmethod
+    def escape_regex(s: str) -> str:
+        """
+        Process string s by escaping special regex characters with backslash
+
+        :param s: string to be processed
+        :return: escaped string
+        """
+        chars_to_be_escaped = ("<", ">", "(", ")", "[", "]", "+", "-", "*", ".", "|", "?", "^")
+
+        escaped_s = []
+        for ch in s:
+            if ch in chars_to_be_escaped:
+                escaped_s.append(r"\\")
+            escaped_s.append(ch)
+        return "".join(escaped_s)
+
+    @staticmethod
+    def convert_xml_special(s: str) -> str:
+        """
+        Process string s by converting special XML entities
+
+        :param s: string to be processed
+        :return: converted string
+        """
+        xml_special_entities = {"<": "&lt;", ">": "&gt;", "&": "&amp;", "\"": "&quot;", "'": "&apos;"}
+
+        converted_s = []
+        for ch in s:
+            converted_ch = xml_special_entities.get(ch, ch)
+            converted_s.append(converted_ch)
+        return "".join(converted_s)
+
+
+if __name__ == "__main__":
+    assert len(sys.argv) == 1
+    converter = Converter()
+
+    converter.parse_xtext_grammar(xtext_source_path="../../org.omg.kerml.xtext/src/org/omg/kerml/xtext/KerML.xtext")
+    converter.export_text_mate_language_file(target_path="./vscode/kerml/syntaxes/kerml.tmLanguage.json")
+    converter.export_jetbrains_language_file(target_path="./jetbrains/KerML.xml")
+
+    converter.parse_xtext_grammar(xtext_source_path="../../org.omg.sysml.xtext/src/org/omg/sysml/xtext/SysML.xtext")
+    converter.export_text_mate_language_file(target_path="./vscode/sysml/syntaxes/sysml.tmLanguage.json")
+    converter.export_jetbrains_language_file(target_path="./jetbrains/SysML.xml")

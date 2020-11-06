07684cc50e95117019e41770fdcd658a01ec3121
Ivan Gomes <ivan-gomes@users.noreply.github.com>
2020-11-06 15:07:30 -0800

    ST6RI-288 Add nodejs and dependency installation tests to installers

--- org.omg.sysml.jupyter.installer/install.bat
+++ org.omg.sysml.jupyter.installer/install.bat
@@ -18,7 +18,7 @@ call java -version || goto :error
 
 echo --- Step 3: Installing dependencies into Conda environment ---
 call conda install python=3.* jupyterlab=2.* -y || goto:error
-call conda install graphviz=2.* -c conda-forge -y || goto:error
+call conda install graphviz=2.* nodejs=14.* -c conda-forge -y || goto:error
 
 echo --- Step 4: Testing Python installation ---
 where python
@@ -27,17 +27,31 @@ if errorlevel 1 (
     goto :error
 )
 
-echo --- Step 5: Installing SysML Jupyter kernel ---
+echo --- Step 5: Testing Graphviz installation ---
+where dot
+if errorlevel 1 (
+    echo Graphviz is not installed. Please manually install it.
+    goto :error
+)
+
+echo --- Step 6: Testing Node.js installation ---
+where node
+if errorlevel 1 (
+    echo Node.js is not installed. Please manually install it.
+    goto :error
+)
+
+echo --- Step 7: Installing SysML Jupyter kernel ---
 call jupyter kernelspec remove sysml -f
 for /F usebackq %%A in (`where dot`) do (
   call python "%~dp0\install.py" --sys-prefix --api-base-path=http://sysml2.intercax.com:9000 "--graphviz-path=%%A%%" %* || goto:error
 )
 
-echo --- Step 6: Installing JupyterLab extension for SysML ---
+echo --- Step 8: Installing JupyterLab extension for SysML ---
 call jupyter labextension uninstall jupyterlab-sysml
 call jupyter labextension install "%~dp0\jupyterlab-sysml\package" || goto:error
 
-echo --- Step 7: Running Jupyter environment ---
+echo --- Step 9: Running Jupyter environment ---
 echo To launch JupyterLab you can now run ^"jupyter lab^" from Command Prompt.
 echo Re-running this script is not necessary and will re-install the environment.
 echo(

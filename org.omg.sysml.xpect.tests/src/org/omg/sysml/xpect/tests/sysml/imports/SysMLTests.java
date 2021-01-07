eed27c844fe638408cd46eca93ae1c242e28e961
Miyako Wilson <miyako.wilson@gatech.edu>
2021-01-07 10:07:07 -0500

    updated recursive package import testcases

--- org.omg.sysml.xpect.tests/src/org/omg/sysml/xpect/tests/sysml/imports/SysMLTests.java
+++ org.omg.sysml.xpect.tests/src/org/omg/sysml/xpect/tests/sysml/imports/SysMLTests.java
@@ -2,9 +2,11 @@ package org.omg.sysml.xpect.tests.sysml.imports;
 
 import org.junit.runner.RunWith;
 import org.eclipse.xpect.runner.XpectRunner;
+import org.eclipse.xpect.runner.XpectTestFiles;
 import org.eclipse.xpect.xtext.lib.tests.XtextTests;
 
 @RunWith(XpectRunner.class)
+@XpectTestFiles(fileExtensions = "xt")
 public class SysMLTests extends XtextTests {
 
 }

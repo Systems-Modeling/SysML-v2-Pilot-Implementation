4981408db15a3c0144186f27586bf2e5e1ca54d8
Ed Seidewitz <ed-s@modeldriven.com>
2021-04-16 01:23:45 -0400

    ST6RI-375 Updated KerML Xpect tests.
    - In particular, import.alias.failing tests now pass, and so they have
    been moved back under import.global.

--- org.omg.kerml.xpect.tests/src/org/omg/kerml/xpect/tests/imports/alias/failing/KerMLImportAliasFailingTest.java
+++
@@ -1,9 +0,0 @@
-package org.omg.kerml.xpect.tests.imports.alias.failing;
-
-import org.junit.runner.RunWith;
-import org.eclipse.xpect.runner.XpectRunner;
-import org.eclipse.xpect.xtext.lib.tests.XtextTests;
-
-@RunWith(XpectRunner.class)
-public class KerMLImportAliasFailingTest extends XtextTests {
-}

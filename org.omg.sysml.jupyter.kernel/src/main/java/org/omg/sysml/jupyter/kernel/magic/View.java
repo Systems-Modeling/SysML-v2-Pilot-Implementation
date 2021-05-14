9e67c9261af90b85c5d6b2b3025399dfa5b10603
Ed Seidewitz <ed-s@modeldriven.com>
2021-05-13 22:09:37 -0400

    ST6RI-362 Added MDS copyright notice to View.java.

--- org.omg.sysml.jupyter.kernel/src/main/java/org/omg/sysml/jupyter/kernel/magic/View.java
+++ org.omg.sysml.jupyter.kernel/src/main/java/org/omg/sysml/jupyter/kernel/magic/View.java
@@ -1,6 +1,7 @@
 /*
  * SysML 2 Pilot Implementation
  * Copyright (c) 2020 Mgnite Inc.
+ * Copyright (c) 2021 Model Driven Solutions, Inc.
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU Lesser General Public License as published by

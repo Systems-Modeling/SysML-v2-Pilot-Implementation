912c544bdb3c80d2b151f9b5257ee5c4b8e84638
Ed Seidewitz <ed-s@modeldriven.com>
2021-09-08 23:32:42 -0400

    ST6RI-390 Created utility programs to export models to JSON files.

--- org.omg.kerml.xtext/src/org/omg/kerml/xtext/util/KerML2JSON.java
+++ org.omg.kerml.xtext/src/org/omg/kerml/xtext/util/KerML2JSON.java
@@ -1,7 +1,6 @@
 /*****************************************************************************
  * SysML 2 Pilot Implementation
- * Copyright (c) 2019-2021 Model Driven Solutions, Inc.
- * Copyright (c) 2021 Twingineer LLC
+ * Copyright (c) 2021 Model Driven Solutions, Inc.
  *    
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU Lesser General Public License as published by
@@ -20,56 +19,36 @@
  * 
  * Contributors:
  *  Ed Seidewitz
- *  Ivan Gomes
  * 
  *****************************************************************************/
 package org.omg.kerml.xtext.util;
 
-import java.nio.file.Paths;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
 import java.util.Arrays;
-import java.util.Date;
-
 import org.omg.sysml.lang.sysml.util.SysMLLibraryUtil;
 import org.omg.sysml.util.ElementUtil;
-import org.omg.sysml.util.traversal.facade.impl.ApiElementProcessingFacade;
+import org.omg.sysml.util.traversal.facade.impl.JsonElementProcessingFacade;
 
 /**
- * This is a utility for traversing a SysML model graph and saving each Element that is 
- * visited to a repository using the SysML v2 REST API.
+ * This is a utility for traversing a model graph and exporting each Element that is 
+ * visited to a file using a JSON representation.
  * 
  * @author Ed Seidewitz
  *
  */
-public class KerMLRepositorySaveUtil extends KerMLTraversalUtil {
+public class KerML2JSON extends KerMLTraversalUtil {
 	
-	private String basePath = ApiElementProcessingFacade.DEFAULT_BASE_PATH;
+	public static final String JSON_EXTENSION = "json";
+	
 	private String libraryPath = null;
 	private boolean isAddImplicitGeneralizations = false;
-	private String projectName;
+	private String outputPath = null;
 	
-	public KerMLRepositorySaveUtil() {
+	public KerML2JSON() {
 		super();
 		this.setVerbose(false);
-	}
-	
-	/**
-	 * Return the identifier for the repository Project to which Elements and Relationships were
-	 * saved. (Available after processing.)
-	 * 
-	 * @return	A string representation of the UUID of the Project in the repository
-	 */
-	public String getProjectId() {
-		return ((ApiElementProcessingFacade)this.traversal.getFacade()).getProjectId();
-	}
-	
-	/**
-	 * Return the base path for API access. This is either as set via the command line "-b" option or,
-	 * otherwise, is the ApiElementProcessFacade default.
-	 * 
-	 * @return
-	 */
-	public String getBasePath() {
-		return this.basePath;
 	}
 	
 	/**
@@ -80,41 +59,35 @@ public class KerMLRepositorySaveUtil extends KerMLTraversalUtil {
 	public String getLibraryPath() {
 		return this.libraryPath;
 	}
-	
+
 	/**
-	 * Get the project name that was set from the command line argument.
+	 * Get the output path.
 	 * 
-	 * @return
+	 * @return	the output path
 	 */
-	public String getProjectName() {
-		return this.projectName;
+	public String getOutputPath() {
+		return this.outputPath;
 	}
 	
 	/**
 	 * Process the command line arguments:
 	 * <ul>
-	 * <li> Set the base path if the "-b" option is present.</li>
 	 * <li> Set the library path if the "-l" option is present.</li>
 	 * <li> Set flag to add implicit generalizations if the "-g" option is present.</li>
 	 * <li> Set flag for verbose mode if the "-v" option is present.</li>
-	 * <li> Return the list of arguments with any options removed and the
-	 *      library path (if any) prepended to all arguments other than the
-	 *      first.</li>
+	 * <li> Return the list of arguments with any options removed.</li>
 	 * </ul>
 	 * 
 	 * @param 	args		the original command line arguments
-	 * @return	the arguments without any options and with the library path (if given)
-	 *          prepended to arguments for library model files.
+	 * @return	the arguments without any options.
 	 */
-	protected String[] processArgs(String[] args) {
+	public String[] processArgs(String[] args) {
 		int n = args.length;
 		if (n > 0) {
 			int i = 0;
-			while(("-b".equals(args[i]) || "-l".equals(args[i]) || "-g".equals(args[i]) || "-v".equals(args[i])) && 
+			while(("-l".equals(args[i]) || "-g".equals(args[i]) || "-v".equals(args[i])) && 
 					i + 1 < n) {
-				if ("-b".equals(args[i])) {
-					this.basePath = args[++i];
-				} else if ("-l".equals(args[i])) {
+				if ("-l".equals(args[i])) {
 					this.libraryPath = args[++i];
 					if (!libraryPath.endsWith("/")) {
 						libraryPath += "/";
@@ -129,12 +102,6 @@ public class KerMLRepositorySaveUtil extends KerMLTraversalUtil {
 			if (i < n) {
 				args = Arrays.copyOfRange(args, i, n);
 				
-				if (this.libraryPath != null) {
-					for (int k = 1; k < args.length; k++) {
-						args[k] = this.libraryPath + args[k];
-					}
-				}
-				
 				return args;
 			}
 		}
@@ -142,37 +109,46 @@ public class KerMLRepositorySaveUtil extends KerMLTraversalUtil {
 	}
 	
 	/**
-	 * Initialize the traversal with an ApiElementProcessingFacade to write to a new repository Project.
-	 * Set the project name as the file name from the first argument after any options, stripped 
-	 * of its extension, with the current date/time appended.
+	 * If there is a library path, set the model library directory to it and prepended it to 
+	 * all arguments other than the first.  Set the output path to be the  same as the input path, 
+	 * but with a JSON file extension. Initialize the traversal with an JsonElementProcessingFacade.
 	 * 
 	 * @param 	args		the command line arguments after processing, with options removed
 	 */
-	protected void initialize(String[] args)  {
+	public void initialize(String[] args)  {
 		String libraryPath = this.getLibraryPath();
 		if (libraryPath != null) {
 			SysMLLibraryUtil.setModelLibraryDirectory(libraryPath);
+			for (int k = 1; k < args.length; k++) {
+				args[k] = libraryPath + args[k];
+			}
 		}
 		
-		this.projectName = Paths.get(args[0]).getFileName().toString();
-		int j = this.projectName.indexOf('.');
-		if (j >= 0) {
-			this.projectName = this.projectName.substring(0, j);
+		this.outputPath = args[0];
+		if (this.outputPath.endsWith("/")) {
+			this.outputPath = this.outputPath.substring(0, this.outputPath.length() - 1);
+		} else {
+			int j = this.outputPath.indexOf('.');
+			if (j >= 0) {
+				this.outputPath = this.outputPath.substring(0, j);
+			}
 		}
-		this.projectName += " " + new Date();
-		
-		ApiElementProcessingFacade processingFacade = new ApiElementProcessingFacade(this.projectName, this.getBasePath());	
+		this.outputPath += "." + JSON_EXTENSION;
+
+		JsonElementProcessingFacade processingFacade = new JsonElementProcessingFacade();	
 		processingFacade.setTraversal(this.initialize(processingFacade));
 		processingFacade.setIsVerbose(this.isVerbose);
 	}
 	
 	/**
-	 * Traverse the model and then commit the change set of ElementVersions so obtained.
+	 * After processing, write the JSON tree to the output file given by the outputPath.
+	 * 
+	 * @throws IOException
 	 */
-	@Override
-	public void process() {
-		super.process();
-		((ApiElementProcessingFacade)this.traversal.getFacade()).commit();
+	public void write() throws IOException {
+		Files.newBufferedWriter(Path.of(this.outputPath)).
+			append(((JsonElementProcessingFacade)this.traversal.getFacade()).toJson()).
+			close();
 	}
 	
 	/**
@@ -180,6 +156,7 @@ public class KerMLRepositorySaveUtil extends KerMLTraversalUtil {
 	 * 
 	 * @param 	args		the array of main program arguments
 	 */
+	@Override
 	public void run(String[] args) {
 		args = this.processArgs(args);
 		
@@ -194,28 +171,30 @@ public class KerMLRepositorySaveUtil extends KerMLTraversalUtil {
 					(this.isAddImplicitGeneralizations? " (adding implicit generalizations)... ": "..."));
 			ElementUtil.transformAll(this.resourceSet, this.isAddImplicitGeneralizations);
 			
-			System.out.println("\nBase path is " + this.getBasePath());
+			System.out.print("Processing");
+			this.process();
 			System.out.println();
 			
-			this.process();
-			System.out.println("Saved to Project (" + this.getProjectName() + ") " + this.getProjectId());
-			System.out.println();
+			System.out.println("Writing " + this.outputPath + "...");
+			try {
+				this.write();
+			} catch (IOException e) {
+				throw new RuntimeException(e);
+			}
 		}
 	}
 
 	/**
 	 * The main program reads the KerML resources as given by its arguments and then processes all the input
-	 * resources. Elements from library resources are only saved to the repository if they are referenced
-	 * from an input resource.
+	 * resources. Elements from library resources are only included if they are referenced from an input resource.
 	 * 
 	 * <p>Usage:
 	 * 
-	 * <p>KerMLRepositorySaveUtil [-b base-path-url] [-l library-base-path] [-g] [-v] input-path [library-path library-path...]
+	 * <p>KerML2JSON [-l library-base-path] [-g] [-v] input-path [library-path library-path...]
 	 * 
 	 * <p>where:
 	 * 
 	 * <ul>
-	 * <li>-b base-path-url       gives the URL for the base path to be used for the API endpoint (if none is given, the default is used)</li>
 	 * <li>-l library-base-path   gives the base path to used for reading model library resources</li>
 	 * <li>-g                     specifies that implicit generalizations should be generated (the default is not to)</li>
 	 * <li>-v                     specifies verbose mode (the default is non-verbose)</li>
@@ -226,7 +205,7 @@ public class KerMLRepositorySaveUtil extends KerMLTraversalUtil {
 	 */
 	public static void main(String[] args) {
 		try {
-			new KerMLRepositorySaveUtil().run(args);
+			new KerML2JSON().run(args);
 		} catch (Exception e) {
 			System.out.println("Error: " + e);
 		}
---
+++ org.omg.sysml.xtext/src/org/omg/sysml/xtext/util/SysML2JSON.java
@@ -0,0 +1,63 @@
+/*****************************************************************************
+ * SysML 2 Pilot Implementation
+ * Copyright (c) 2021 Model Driven Solutions, Inc.
+ *    
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of theGNU Lesser General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ *
+ * @license LGPL-3.0-or-later <http://spdx.org/licenses/LGPL-3.0-or-later>
+ * 
+ * Contributors:
+ *  Ed Seidewitz
+ * 
+ *****************************************************************************/
+package org.omg.sysml.xtext.util;
+
+import org.omg.kerml.xtext.util.KerML2JSON;
+import org.omg.sysml.xtext.SysMLStandaloneSetup;
+
+public class SysML2JSON extends KerML2JSON {
+
+	public SysML2JSON() {
+		super();
+		SysMLStandaloneSetup.doSetup();
+		this.addExtension(".sysml");
+	}
+	
+	/**
+	 * The main program reads the KerML and SysML resources as given by its arguments and then processes all the input
+	 * resources. Elements from library resources are only included if they are referenced from an input resource.
+	 * 
+	 * <p>Usage:
+	 * 
+	 * <p>SysML2JSON [-l library-base-path] [-g] input-path [library-path library-path...]
+	 * 
+	 * <p>where:
+	 * 
+	 * <ul>
+	 * <li>-l library-base-path   gives the base path to used for reading model library resources</li>
+	 * <li>-g                     specifies that implicit generalizations should be generated (the default is not to)</li>
+	 * <li>input-path             is a path for reading input resources</li>
+	 * <li>library-paths          are paths for reading library resources, relative to the library-base-path (if one is given)</li>
+	 * </ul>
+	 * 
+	 */
+	public static void main(String[] args) {
+		try {
+			new SysML2JSON().run(args);
+		} catch (Exception e) {
+			System.out.println("Error: " + e);
+		}
+	}
+
+}

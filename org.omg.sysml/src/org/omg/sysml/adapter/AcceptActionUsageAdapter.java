b8d377cf7813d1ab18bf85d734808a6e946e9a4b
Ed Seidewitz <ed-s@modeldriven.com>
2022-06-08 17:02:38 -0400

    ST6RI-545 Removed default receiver transform from AcceptActionUsageAdapter.

--- org.omg.sysml/src/org/omg/sysml/adapter/AcceptActionUsageAdapter.java
+++ org.omg.sysml/src/org/omg/sysml/adapter/AcceptActionUsageAdapter.java
@@ -1,11 +1,7 @@
 package org.omg.sysml.adapter;
 
 import org.omg.sysml.lang.sysml.AcceptActionUsage;
-import org.omg.sysml.lang.sysml.ActionDefinition;
-import org.omg.sysml.lang.sysml.ActionUsage;
 import org.omg.sysml.lang.sysml.Element;
-import org.omg.sysml.lang.sysml.Feature;
-import org.omg.sysml.util.UsageUtil;
 
 public class AcceptActionUsageAdapter extends ActionUsageAdapter {
 
@@ -32,39 +28,4 @@ public class AcceptActionUsageAdapter extends ActionUsageAdapter {
 		super.addComputedRedefinitions(skip);
 	}
 
-	// Transformation
-	
-	/**
-	 * The default receiver is the outermost containing ActionUsage of this AcceptActionUsage
-	 * (or this AcceptActionUsage if it is not contained in any other ActionUsage), unless that
-	 * ActionUsage is itself owned by an ActionDefinition.
-	 */
-	// TODO: Determine the proper default receiver for an AcceptActionUsage contained in an 
-	// ActionDefinition or performed by a PartDefinition or PartUsage.
-	protected Feature getDefaultReceiver() {
-		Feature receiver = getTarget();
-		Element owner = receiver.getOwner();
-		while (owner instanceof ActionUsage) {
-			receiver = (Feature)owner;
-			owner = receiver.getOwner();
-		}
-		return owner instanceof ActionDefinition? getTarget(): receiver;
-	}
-	
-	public void addDefaultReceiverBinding() {
-		AcceptActionUsage target = getTarget();
-		if (target.getReceiverArgument() == null) {
-			Feature receiverParameter = UsageUtil.getReceiverParameterOf(target);
-			if (receiverParameter != null) {
-				addBindingConnector(getDefaultReceiver(), receiverParameter);
-			}
-		}
-	}
-	
-	@Override
-	public void doTransform() {
-		super.doTransform();
-		addDefaultReceiverBinding();
-	}
-	
 }

96618c9de67fba01a5542aef1128b4ace0cfa1ef
Tilo Schreiber <tilo.schreiber@siemens.com>
2022-06-15 10:34:07 -0700

    ST6RI-500 Added Siemens copyright in setting delegates where missing

--- org.omg.sysml/src/org/omg/sysml/delegate/TransitionUsage_succession_SettingDelegate.java
+++ org.omg.sysml/src/org/omg/sysml/delegate/TransitionUsage_succession_SettingDelegate.java
@@ -1,6 +1,6 @@
 /*******************************************************************************
  * SysML 2 Pilot Implementation
- * Copyright (c) 2022 Model Driven Solutions, Inc.
+ * Copyright (c) 2022 Siemens AG
  *    
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU Lesser General Public License as published by

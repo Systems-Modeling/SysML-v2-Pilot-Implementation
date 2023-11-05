541b2c13c976e7aef1fab41a1cdc9b41a3c6e8eb
Hisashi Miyashita <himi@mgnite.com>
2023-11-04 22:32:15 -0400

    Updated the comments

--- org.omg.sysml.plantuml/src/org/omg/sysml/plantuml/InheritKey.java
+++ org.omg.sysml.plantuml/src/org/omg/sysml/plantuml/InheritKey.java
@@ -1,6 +1,6 @@
 /*****************************************************************************
  * SysML 2 Pilot Implementation, PlantUML Visualization
- * Copyright (c) 2022 Mgnite Inc.
+ * Copyright (c) 2022-2023 Mgnite Inc.
  *    
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU Lesser General Public License as published by

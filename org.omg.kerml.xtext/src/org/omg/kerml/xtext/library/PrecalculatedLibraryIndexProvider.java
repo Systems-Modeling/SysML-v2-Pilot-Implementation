3284a93a490725b032c39f26cb0404c29708d792
Hisashi Miyashita <himi@mgnite.com>
2024-10-02 16:02:07 -0400

    Removed the unused import, java.util.Arrays.

--- org.omg.kerml.xtext/src/org/omg/kerml/xtext/library/PrecalculatedLibraryIndexProvider.java
+++ org.omg.kerml.xtext/src/org/omg/kerml/xtext/library/PrecalculatedLibraryIndexProvider.java
@@ -27,7 +27,6 @@ import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.InputStreamReader;
 import java.nio.charset.StandardCharsets;
-import java.util.Arrays;
 
 import org.apache.log4j.Logger;
 import org.eclipse.emf.common.util.URI;

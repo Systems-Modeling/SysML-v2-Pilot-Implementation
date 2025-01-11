cd97bc98a43610ea4dd5b7817827282c06f1628d
Ed Seidewitz <ed-s@modeldriven.com>
2025-01-11 03:39:18 -0500

    SYSM2L_-89 Added special semantics tests for checkTransitionStateUsageSpecialization.

--- org.omg.sysml.interactive.tests/src/org/omg/sysml/semantics/tests/SysMLSpecializationSpecialTest.java
+++ org.omg.sysml.interactive.tests/src/org/omg/sysml/semantics/tests/SysMLSpecializationSpecialTest.java
@@ -1,6 +1,6 @@
 /**
  * SysML 2 Pilot Implementation
- * Copyright (C) 2024 Model Driven Solutions, Inc.
+ * Copyright (C) 2024-2025 Model Driven Solutions, Inc.
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU Lesser General Public License as published by
@@ -19,11 +19,26 @@
  * 
  * Contributors:
  *   Laszlo Gati, MDS
+ *   Ed Seidewitz, MDS
  */
 package org.omg.sysml.semantics.tests;
 
+import static org.junit.Assert.assertTrue;
+
+import org.eclipse.emf.ecore.EClass;
+import org.eclipse.emf.ecore.EClassifier;
 import org.junit.Ignore;
 import org.junit.Test;
+import org.omg.sysml.lang.sysml.Element;
+import org.omg.sysml.lang.sysml.StateUsage;
+import org.omg.sysml.lang.sysml.SysMLFactory;
+import org.omg.sysml.lang.sysml.SysMLPackage;
+import org.omg.sysml.lang.sysml.TransitionUsage;
+import org.omg.sysml.lang.sysml.Type;
+import org.omg.sysml.lang.sysml.util.SysMLLibraryUtil;
+import org.omg.sysml.util.ElementUtil;
+import org.omg.sysml.util.NamespaceUtil;
+import org.omg.sysml.util.TypeUtil;
 
 /**
  * Semantic constraint tests for "Implied Definition Subclassification Relationships" and
@@ -32,6 +47,49 @@ import org.junit.Test;
  * Cases that cannot be covered by the parameterized test class {@link SysMLSpecializationTest}
  */
 public class SysMLSpecializationSpecialTest extends SysMLSemanticTest {
+	
+	protected void checkTransitionUsageStateSpecialization(String owningTypeName) {
+		//create and add root package
+		org.omg.sysml.lang.sysml.Package root = SysMLFactory.eINSTANCE.createPackage();
+		getResource().getContents().add(root);
+		
+		//check if library element exists
+		Element libraryElement = SysMLLibraryUtil.getLibraryElement(root, "States::StateAction::stateTransitions");
+		assertTrue(libraryElement instanceof Type);
+
+		//create owning type
+		EClassifier ownerEClass = SysMLPackage.eINSTANCE.getEClassifier(owningTypeName);
+		Type owner = (Type) SysMLFactory.eINSTANCE.create((EClass) ownerEClass);
+		NamespaceUtil.addOwnedMemberTo(root, owner);
+		
+		//create source state usage
+		StateUsage state = SysMLFactory.eINSTANCE.createStateUsage();
+		state.setIsReference(false);
+		TypeUtil.addOwnedFeatureTo(owner, state);
+		
+		//create transition usage
+		TransitionUsage transition = SysMLFactory.eINSTANCE.createTransitionUsage();
+		transition.setIsReference(false);
+		TypeUtil.addOwnedFeatureTo(owner, transition);
+		
+		//add source state usage to transition usage
+		NamespaceUtil.addMemberTo(transition, state);
+		
+		//run transformation, add implicit elements
+		ElementUtil.transformAll(root, true);
+		
+		assertTrue(String.format("TransitionUsage specializes stateTransitions instead of %s", getSpecifics(transition)), transition.specializes((Type)libraryElement));
+	}
+	
+	@Test
+	public void checkTransitionUsageStateSpecialization_StateDefinition() {
+		checkTransitionUsageStateSpecialization("StateDefinition");
+	}
+	
+	@Test
+	public void checkTransitionUsageStateSpecialization_StateUsage() {
+		checkTransitionUsageStateSpecialization("StateUsage");
+	}
 	
 	@Test
 	@Ignore("TBD")

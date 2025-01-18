966f123d914d13aadf2d0171dd5c021fa282b4fc
Ed Seidewitz <ed-s@modeldriven.com>
2025-01-18 17:40:21 -0500

    ST6RI-815 Fixed SysMLBinaryRelationTest and SysMLImpliedRelationsTest.

--- org.omg.sysml.interactive.tests/src/org/omg/sysml/semantics/tests/SysMLBinaryRelationTest.java
+++ org.omg.sysml.interactive.tests/src/org/omg/sysml/semantics/tests/SysMLBinaryRelationTest.java
@@ -1,6 +1,6 @@
 /**
  * SysML 2 Pilot Implementation
- * Copyright (C) 2024 Model Driven Solutions, Inc.
+ * Copyright (C) 2024,2025 Model Driven Solutions, Inc.
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU Lesser General Public License as published by
@@ -133,7 +133,7 @@ public class SysMLBinaryRelationTest extends SysMLSemanticTest {
 	public static void addEndTo(Type type) {
 		var end = SysMLFactory.eINSTANCE.createFeature();
 		var endMembership = SysMLFactory.eINSTANCE.createEndFeatureMembership();
-		endMembership.setFeature(end);
+		endMembership.setOwnedMemberFeature(end);
 		type.getOwnedRelationship().add(endMembership);
 	}
 }

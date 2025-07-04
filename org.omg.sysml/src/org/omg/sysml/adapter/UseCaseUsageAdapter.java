33c9174edd82f0ad15eec04ac4e6e8c8389da8de
Ed Seidewitz <ed-s@modeldriven.com>
2025-07-04 14:07:21 -0400

    ST6RI-864 Properly satisfied checkIncludeUseCaseUsageSpecialization.
    - Also turned on tests for IncludeUseCaseUsage in
    SysMLSpecializationTest.

--- org.omg.sysml/src/org/omg/sysml/adapter/UseCaseUsageAdapter.java
+++ org.omg.sysml/src/org/omg/sysml/adapter/UseCaseUsageAdapter.java
@@ -1,6 +1,6 @@
 /*******************************************************************************
  * SysML 2 Pilot Implementation
- * Copyright (c) 2021, 2023 Model Driven Solutions, Inc.
+ * Copyright (c) 2021, 2023, 2025 Model Driven Solutions, Inc.
  *    
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU Lesser General Public License as published by

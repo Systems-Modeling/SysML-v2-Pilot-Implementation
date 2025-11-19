b9c88bee35fba46274766a4ed261bf4ca6b14675
Hans Peter de Koning <32621328+hpdekoning@users.noreply.github.com>
2025-11-19 15:45:04 +0100

    feat: Added basic textual BNF grammar parser tool for file-based grammar validation

---
+++ tool-support/bnf_grammar_tools/kerml_sysml_bnf_parser.py
@@ -0,0 +1,73 @@
+import os
+import sys
+import json
+import re
+import argparse
+from datetime import datetime, timezone
+from dataclasses import dataclass
+from enum import Enum, auto
+from textwrap import wrap
+from typing import Optional, Iterable, Any
+
+from lark import Lark, Transformer, Tree, UnexpectedInput
+
+# Create logger for debug, info, warning, error, critical messages
+import logging
+LOGGER = logging.getLogger()
+
+
+class BnfParser:
+    def __init__(self) -> None:
+        self.bnf_filepath: Optional[str] = None
+        self.parser: Optional[Lark] = None
+
+    def parse(self, bnf_filepath: str) -> None:
+        self.bnf_filepath = bnf_filepath
+        self.parser = Lark.open("kerml_sysml_textual_grammar.lark", rel_to=__file__, parser="lalr")
+
+        bnf_file = open(bnf_filepath, "r", encoding="utf-8")
+        bnf_input = bnf_file.read()
+        bnf_file.close()
+
+        try:
+            parse_tree = self.parser.parse(bnf_input)
+        except UnexpectedInput as e:
+            LOGGER.error(f"Parse error in {self.bnf_filepath}:\n{e}")
+        else:
+            LOGGER.info(f"Parse completed successfully")
+            LOGGER.info(f"Resulting parse tree:\n\n{parse_tree.pretty()}")
+
+
+def main() -> None:
+    # Initialize logging
+    LOGGER.setLevel(logging.DEBUG)
+    formatter = logging.Formatter("%(levelname)-8s: %(message)s")
+
+    console_handler = logging.StreamHandler()
+    console_handler.set_name("console")
+    console_handler.setLevel(logging.INFO)
+    console_handler.setFormatter(formatter)
+    LOGGER.addHandler(console_handler)
+
+    file_handler = logging.FileHandler("kerml_sysml_bnf_parser.log", mode="w", encoding="utf-8")
+    file_handler.set_name("logfile")
+    file_handler.setLevel(logging.INFO)
+    file_handler.setFormatter(formatter)
+    LOGGER.addHandler(file_handler)
+
+    # Parse command line
+    parser = argparse.ArgumentParser(
+        prog="kerml_sysml_bnf_parser",
+        allow_abbrev=False,
+        description="Parse textual and/or graphical KerML or SysML BNF grammars.")
+    parser.add_argument("bnf_path", metavar="BNF_PATH", type=str, help="Path to BNF text file")
+    args = parser.parse_args()
+    LOGGER.debug(f"args={args}")
+    LOGGER.info(f"cwd={os.getcwd()}")
+
+    bnf_parser = BnfParser()
+    bnf_parser.parse(args.bnf_path)
+
+
+if __name__ == "__main__":
+    main()

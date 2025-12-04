9178ff00c19bfac6bb74afc2daab287d3b7f797c
Hans Peter de Koning <32621328+hpdekoning@users.noreply.github.com>
2025-12-04 22:45:24 +0100

    fix: Refactoring and major upgrade of the bnf_grammar_processor and bnf_file_parser tool, that now completely implement the workflow described in the README documentation

--- tool-support/bnf_grammar_tools/bnf_grammar/bnf_file_parser.py
+++ tool-support/bnf_grammar_tools/bnf_grammar/bnf_file_parser.py
@@ -1,15 +1,30 @@
-import os
+#!python
+
+"""
+bnf_file_parser is a command line tool that parses a KerML or SysML plain text grammar file.
+
+The supported file formats are:
+- .kebnf for a KerML or SysML textual notation grammar
+- .kgbnf for a SysML graphical notation grammar
+
+Its usage is described below in the main() function.
+
+@author: Hans Peter de Koning (DEKonsult)
+
+Requirements:
+
+This tool requires installation of the following packages:
+- lark (See https://pypi.org/project/lark)
+
+"""
+
 import sys
-import json
-import re
+import os
+import shutil
 import argparse
 from datetime import datetime, timezone
-from dataclasses import dataclass
-from enum import Enum, auto
-from textwrap import wrap
-from typing import Optional, Iterable, Any
-
-from lark import Lark, Transformer, Tree, UnexpectedInput
+from typing import Optional
+from lark import Lark, UnexpectedInput
 
 # Create logger for debug, info, warning, error, critical messages
 import logging
@@ -18,11 +33,16 @@ LOGGER = logging.getLogger()
 
 class BnfParser:
     def __init__(self) -> None:
+        self.start_timestamp: Optional[datetime] = None
         self.bnf_filepath: Optional[str] = None
         self.parser: Optional[Lark] = None
 
     def parse(self, bnf_filepath: str) -> None:
+        self.start_timestamp = datetime.now(timezone.utc).isoformat(timespec="seconds").replace("+00:00", "Z")
         self.bnf_filepath = bnf_filepath
+
+        LOGGER.info(f"Started parsing {self.bnf_filepath} at {self.start_timestamp}")
+
         basename, ext = os.path.splitext(bnf_filepath)
         grammar_file = None
         if ext == ".kebnf":
@@ -30,7 +50,8 @@ class BnfParser:
         elif ext == ".kgbnf":
             grammar_file = "kgbnf_graphical_grammar.lark"
         else:
-            LOGGER.critical(f"Unrecognized file extension for BNF_PATH {bnf_filepath}")
+            LOGGER.critical(f"Unrecognized file extension for BNF_PATH {bnf_filepath}, terminating ...")
+            sys.exit(1)
 
         self.parser = Lark.open(grammar_file, rel_to=__file__, parser="lalr")
 
@@ -44,7 +65,7 @@ class BnfParser:
             LOGGER.error(f"Parse error in {self.bnf_filepath}:\n{e}")
         else:
             LOGGER.info(f"Parse completed successfully")
-            LOGGER.info(f"Resulting parse tree:\n\n{parse_tree.pretty()}")
+            LOGGER.info(f"The resulting (AST) parse tree is:\n\n{parse_tree.pretty()}")
 
 
 def main() -> None:
@@ -58,22 +79,29 @@ def main() -> None:
     console_handler.setFormatter(formatter)
     LOGGER.addHandler(console_handler)
 
-    file_handler = logging.FileHandler("kerml_sysml_bnf_parser.log", mode="w", encoding="utf-8")
+    log_file_name = "bnf_file_parser.log"
+    if os.path.exists(log_file_name):
+        # Create backup copy of the log-file to inspect differences between runs
+        shutil.copy2(log_file_name, log_file_name + ".backup")
+
+    file_handler = logging.FileHandler(log_file_name, mode="w", encoding="utf-8")
     file_handler.set_name("logfile")
     file_handler.setLevel(logging.INFO)
     file_handler.setFormatter(formatter)
     LOGGER.addHandler(file_handler)
 
+    LOGGER.debug(f"bnf_grammar_parser started in {os.getcwd()}")
+
     # Parse command line
     parser = argparse.ArgumentParser(
-        prog="kerml_sysml_bnf_parser",
+        prog="bnf_file_parser",
         allow_abbrev=False,
-        description="Parse textual and/or graphical KerML or SysML BNF grammars.")
+        description="Parse KerML or SysML grammar files in textual or graphical BNF format.")
     parser.add_argument("bnf_path", metavar="BNF_PATH", type=str, help="Path to plain text BNF file with extension .kebnf or .kgbnf")
     args = parser.parse_args()
     LOGGER.debug(f"args={args}")
-    LOGGER.debug(f"cwd={os.getcwd()}")
 
+    # Run the parser
     bnf_parser = BnfParser()
     bnf_parser.parse(args.bnf_path)
 

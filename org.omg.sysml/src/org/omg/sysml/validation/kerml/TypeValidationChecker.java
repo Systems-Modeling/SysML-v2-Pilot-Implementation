212ae8ccf758df631da2755c15c3c7ae74e68712
geneShreve <gene.shreve@i3-corps.com>
2026-04-20 16:43:59 -0500

    ST6RI-904 Completed all Type Validation Checker functions in TypeValidationChecker.java - needs review

--- org.omg.sysml/src/org/omg/sysml/validation/kerml/TypeValidationChecker.java
+++ org.omg.sysml/src/org/omg/sysml/validation/kerml/TypeValidationChecker.java
@@ -1,7 +1,13 @@
 package org.omg.sysml.validation.kerml;
 
 import org.omg.sysml.lang.sysml.Element;
+//import org.omg.sysml.lang.sysml.Relationship;
 import org.omg.sysml.validation.ValidationMessageAccepter;
+import org.omg.sysml.lang.sysml.Conjugation;
+import org.omg.sysml.lang.sysml.Differencing;
+import org.omg.sysml.lang.sysml.Intersecting;
+import org.omg.sysml.lang.sysml.Multiplicity;
+import org.omg.sysml.lang.sysml.Unioning;
 
 public class TypeValidationChecker extends NamespaceValidationChecker {
 	
@@ -19,35 +25,69 @@ public class TypeValidationChecker extends NamespaceValidationChecker {
 	}
 						
 	public void validateTypeAtMostOneConjugator(Element element, ValidationMessageAccepter messageAccepter) {
-		
+			
+		//get the number of conjugate relationships
+		long conjCount = element.getOwnedRelationship().stream().filter(Conjugation.class::isInstance).count();
+		if (conjCount > 1) {
+			messageAccepter.error(element, null, "validateTypeMostOneConjugator");
+		}	
 	}
 	
 	public void validateTypeDifferencingTypesNotSelf(Element element, ValidationMessageAccepter messageAccepter) {
 		
+	 //get a list of Differencing objects and check if target is itself  
+		boolean hasSelf = element.getOwnedRelationship().stream().filter(Differencing.class::isInstance).anyMatch(rel -> rel.getTarget().contains(element));
+		if (hasSelf) {
+			messageAccepter.error(element, null,  "validateTypeDifferencingTypesNotSelf");
+		}
 	}
 	
 	public void validateTypeIntersectingTypesNotSelf(Element element, ValidationMessageAccepter messageAccepter) {
 		
+		boolean intersectSelf = element.getOwnedRelationship().stream().filter(Intersecting.class::isInstance).anyMatch(rel -> rel.getTarget().contains(element));
+		if (intersectSelf) {
+			messageAccepter.error(element, null,  "validateTypeIntersectingTypesNotSelf");
+		}
 	}
 	
 	public void validateTypeOwnedDifferencingNotOne(Element element, ValidationMessageAccepter messageAccepter) {
 		
+		long diffCount = element.getOwnedRelationship().stream().filter(Differencing.class::isInstance).count();
+		if (diffCount == 1) {
+			messageAccepter.error(element, null, "validateOwnedDifferencingNotOne");
+		}	
 	}
 	
 	public void validateTypeOwnedIntersectingNotOne(Element element, ValidationMessageAccepter messageAccepter) {
 		
+		long intersectCount = element.getOwnedRelationship().stream().filter(Intersecting.class::isInstance).count(); 
+		if (intersectCount ==1) {
+			messageAccepter.error(element, null, "validateOwnedIntersectingNotOne");
+		}	
 	}
 	
 	public void validateTypeOwnedMultiplicity(Element element, ValidationMessageAccepter messageAccepter) {
 		
+		long multiCount = element.getOwnedRelationship().stream().filter(Multiplicity.class::isInstance).count(); 
+		if (multiCount > 1) {
+			messageAccepter.error(element, null, "validateTypeOwnedMultiplicity");
+		}
 	}
 	
 	public void validateTypeOwnedUnioningNotOne(Element element, ValidationMessageAccepter messageAccepter) {
 		
+		long unionCount = element.getOwnedRelationship().stream().filter(Unioning.class::isInstance).count(); 
+		if (unionCount > 1) {
+			messageAccepter.error(element, null, "validateOwnedUnioningNotOne");
+		}
 	}
 	
 	public void validateTypeUnioningTypesNotSelf(Element element, ValidationMessageAccepter messageAccepter) {
 		
+		boolean unionSelf = element.getOwnedRelationship().stream().filter(Unioning.class::isInstance).anyMatch(rel -> rel.getTarget().contains(element));
+		if (unionSelf) {
+			messageAccepter.error(element, null,  "validateTypeUnioningTypesNotSelf");
+		}
 	}
 	
 }
